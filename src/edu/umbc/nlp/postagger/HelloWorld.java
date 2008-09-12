1c40fe2f849aa6431b5857679eee132a0ed5a5c3
dave.feltenberger <dave.feltenberger@635382c6-8109-11dd-88bd-cff828a63736>
2008-09-12 20:44:20 +0000

    setting project structure up.

---
+++ src/edu/umbc/nlp/postagger/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package edu.umbc.nlp.postagger;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Blah.
+ * @author dave
+ *
+ */
+public class HelloWorld {
+	private static final Logger log = Logger.getLogger(HelloWorld.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		log.info("Hello World!");
+	}
+
+}
