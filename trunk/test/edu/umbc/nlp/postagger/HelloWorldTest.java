0c18b7900055907baeb2fc88f66001ecf1b7df1e
dave.feltenberger <dave.feltenberger@635382c6-8109-11dd-88bd-cff828a63736>
2008-09-12 23:25:11 +0000

    added sample test case

---
+++ trunk/test/edu/umbc/nlp/postagger/HelloWorldTest.java
@@ -0,0 +1,26 @@
+package edu.umbc.nlp.postagger;
+
+import junit.framework.TestCase;
+
+/**
+ * asdf.
+ * @author dave
+ *
+ */
+public class HelloWorldTest extends TestCase {
+
+	protected void setUp() throws Exception {
+	}
+
+	protected void tearDown() throws Exception {
+	}
+
+	/**
+	 * Stub test...
+	 * @throws Exception
+	 */
+	public void testHelloWorld() throws Exception {
+		assertEquals("The test succeeded...wahoo!", 1,  1);
+	}
+
+}
